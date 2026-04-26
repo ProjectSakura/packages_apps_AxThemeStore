885b6b3642d5b189b04fc739c3759adb3abe29ce
Saikrishna1504 <saikrishna26918@gmail.com>
2026-04-26 20:46:15 +0000

    Fix navigation bar overlap
    Change-Id: Ibe61cf84b99aaf9382580b1a0c814bbb5bcb707c
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- src/com/android/axion/axthemestore/MainActivity.kt
+++ src/com/android/axion/axthemestore/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -40,7 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AxThemeStoreTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().navigationBarsPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     MainScreen(viewModel = viewModel)
